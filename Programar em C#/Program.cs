1ab2a0a1144afef75b9ba77cd01e50caaccb5aae
dev <web2ajax@gmail.com>
2018-07-14 21:51:09 -0300

    first commit

---
+++ Programar em C#/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Base {
+    class Program {
+        static void Main(string[] args) {
+            var video = new ClasseDoVideoEmQuestao();
+            video.Run();
+            Console.ReadKey();
+        }
+    }
+}
